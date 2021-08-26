e1f745f63ffcb88cf5b95099bbe7d68a1b49bb6b
Shim MunSeong <Puterism.K@gmail.com>
2021-08-26 17:19:45 +0900

    chore: `darass-react` 적용

--- src/components/comment/comment.tsx
+++ src/components/comment/comment.tsx
@@ -1,27 +1,8 @@
 import * as React from 'react';
-import { useEffect } from 'react';
-import { Helmet } from 'react-helmet';
+import Darass from 'darass-react';
 
 const Comment = () => {
-  useEffect(() => {
-    const $document = document;
-
-    const $script = $document.createElement('script');
-    $script.src = 'https://dorvcm7xtbd6v.cloudfront.net/embed.js';
-    $script.defer = true;
-
-    $document.head.appendChild($script);
-
-    return () => {
-      $script.remove();
-    };
-  }, []);
-
-  return (
-    <div id="darass" data-project-key="OW98x3jymJuQZWI22">
-      <noscript>다라쓰 댓글 작성을 위해 JavaScript를 활성화 해주세요</noscript>
-    </div>
-  );
+  return <Darass projectKey="JfyHTUoM1GfL6KP30" />;
 };
 
 export default Comment;
